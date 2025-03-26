baa11b66402144ee971e86f5d489af121f5d94de
dmamanipar <dmamanipar@unsa.edu.pe>
2025-03-26 08:54:45 -0500

    config server

--- ms_catalogo/src/main/java/com/example/ms_catalogo/MsCatalogoApplication.java
+++ ms_catalogo/src/main/java/com/example/ms_catalogo/MsCatalogoApplication.java
@@ -1,7 +1,11 @@
 package com.example.ms_catalogo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsCatalogoApplication {
@@ -9,5 +13,6 @@ public class MsCatalogoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsCatalogoApplication.class, args);
     }
+
 
 }
