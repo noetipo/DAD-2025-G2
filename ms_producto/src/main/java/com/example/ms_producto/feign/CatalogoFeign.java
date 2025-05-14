73d8e4d695ca8683f05cd49e5f20f553beb4e418
Noe Wilber <noe.tipo@gmail.com>
2025-05-14 08:31:38 -0500

    cambios

--- ms_producto/src/main/java/com/example/ms_producto/feign/CatalogoFeign.java
+++ ms_producto/src/main/java/com/example/ms_producto/feign/CatalogoFeign.java
@@ -1,6 +1,7 @@
 package com.example.ms_producto.feign;
 
 import com.example.ms_producto.dto.CategoriaDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-catalogo-service", path = "/categorias")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "categoriaListarPorIdCB", fallbackMethod = "fallbackProductoById")
     public ResponseEntity<CategoriaDto> buscarCategoria(@PathVariable Integer id);
+
+    default ResponseEntity<CategoriaDto> fallbackProductoById(Integer id, Exception e) {
+        CategoriaDto categoriaDto = new CategoriaDto();
+        categoriaDto.setNombre("Servicio no disponible de categoria");
+        return ResponseEntity.ok(categoriaDto);
+    }
+
 }
